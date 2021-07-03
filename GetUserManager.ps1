99edc476b5fbb0eb337a3f03b3cf6e2e622cc46b
mash1981 <86870607+mash1981@users.noreply.github.com>
2021-07-03 09:55:09 +0200

    Added GetUserManager
    This is a powershell script to get the requesting users manager from AD which can be used in the install software workflow of Snow Productivity Optimizer (aka Automation Platform). You add this script as a powershell web service and can use it in the install software workflow.

---
+++ GetUserManager.ps1
@@ -0,0 +1,23 @@
+﻿function GetUserManager {
+param ($userid)
+	
+	if ($userid.Contains("\"))	{
+		$userid = $userid.Split("\")[1]
+	}
+
+	#import-module activedirectory
+	$user = Get-ADUser -Identity $userid -properties manager
+
+	if (-not $user.manager) {   
+		$manager = new-object PSCustomObject
+		#$manager | Add-Member -Type NoteProperty -Name "myDisplayName" - Value "Application approval group" -Force   
+		#$manager | Add-Member -Type NoteProperty -Name "samAccountName" - Value "..." - Force     
+		$manager | Add-Member -Type NoteProperty -Name "myDisplayName" -Value "No manager found in AD" -Force
+		$manager | Add-Member -Type NoteProperty -Name "samAccountName" -Value "" -Force
+	}   
+	else {
+		$manager = Get-ADUser -Identity $user.manager -properties displayName
+		$manager | Add-Member -Type NoteProperty -Name "myDisplayName" -Value "$($manager.displayName) ($($manager.samAccountName))" -Force
+	}
+	$manager
+}
